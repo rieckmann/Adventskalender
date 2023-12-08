8642552268c5b764c94e5cbb0b45797ddd820187
Matthias Rieckmann <Matthias.Rieckmann@fdy.tu-darmstadt.de>
2023-12-08 15:29:17 +0100

    optimizing

--- 2023/A08.m
+++ 2023/A08.m
@@ -2,40 +2,43 @@ clc
 clear all
 
 data = readlines("A08.txt");
+tic
 instructions = arrayfun(@str2num ,regexprep(data{1},{'L','R'},{'2','3'}));
 map = regexprep(data(3:end), {'[=(),]', '  '},{'', ' '}).split(' ');
+nummap = [(1:1:size(map,1))', arrayfun(@(x) find(map(:,1)==x),map(:,2)), arrayfun(@(x) find(map(:,1)==x),map(:,3))];
+L = length(instructions);
 
-node = "AAA";
-step = 0;
-while node ~= "ZZZ"
-    idx = find(map(:,1) == node);
-    node = map(idx, instructions(mod(step,length(instructions))+1));
-    step = step+1;
-end
-disp(['Part 1 solution: ', num2str(step)])
-
-% to solve this we use that also not explicitly given in the exercise text,
-% we arrive after a full set of instructions at a terminal node and each
-% start node terminates after executing a prime number of instruction sets
-nummap = arrayfun(@str2num ,regexprep(map, map(:,1)', cellfun(@num2str, num2cell(1:1:size(map,1)), 'UniformOutput', false)));
 sequencemap = nummap(:,1);
 for i=1:length(instructions)
     sequencemap = nummap(nummap(:,1), instructions(i));
 end
+
+idx = find(map(:,1) == "AAA");
+terminalidx = find(map(:,1) == "ZZZ");
+step = 0;
+while idx ~= terminalidx
+    idx = sequencemap(idx);
+    step = step+1;
+end
+disp(['Part 1 solution: ', num2str(step*L)])
+
+% to solve this we use that also not explicitly given in the exercise text,
+% we arrive after a full set of instructions at a terminal node and each
+% start node terminates after executing a prime number of instruction sets
 nodesA = nummap(cellfun(@(x) ~isempty(x) ,regexp(map(:,1), 'A$')),1);
 terminalnodes = cellfun(@(x) isempty(x) ,regexp(map(:,1), 'Z$'));
-lcm = 1;
-for i=1:6
+iter = 1;
+for i=1:size(nodesA,1)
     node = nodesA(i);
     step2 = 0;
     while terminalnodes(node) ~= 0
         node = sequencemap(node);
         step2 = step2+1;
     end
-    lcm = lcm * step2; 
+    iter = lcm(step2, iter); 
 end
-disp(['Part 2 solution: ', num2str(lcm*length(instructions))])
-
+disp(['Part 2 solution: ', num2str(iter*length(instructions))])
+toc
 
 
 
